d5039f58d457d94f9432e2a476bc6a7faf69aa76
Antonio Benjumea <73562533+antjes88@users.noreply.github.com>
2025-06-21 13:11:41 +0100

    add irr per company and underlying product (#2)

--- models/tier2_staging/irr/cashflows.sql
+++ models/tier2_staging/irr/cashflows.sql
@@ -5,6 +5,7 @@
 
 SELECT
   entity_name,
+  --'accounts' as ledger_name,
   first_day_of_month,
   value,
   inflow,
@@ -13,6 +14,7 @@ FROM {{ ref("irr_all_accounts") }}
 UNION ALL
 SELECT
   entity_name,
+  --'total_portfolio' as ledger_name,
   first_day_of_month,
   value,
   inflow,
@@ -21,6 +23,7 @@ FROM {{ ref("irr_total_portfolio") }}
 UNION ALL
 SELECT
   entity_name,
+  --'total_funds' as ledger_name,
   first_day_of_month,
   value,
   inflow,
@@ -29,6 +32,7 @@ FROM {{ ref("irr_total_funds") }}
 UNION ALL
 SELECT
   entity_name,
+  --'total_shares' as ledger_name,
   first_day_of_month,
   value,
   inflow,
@@ -37,8 +41,27 @@ FROM {{ ref("irr_total_shares") }}
 UNION ALL
 SELECT
   entity_name,
+  --'total_private_pension' as ledger_name,
   first_day_of_month,
   value,
   inflow,
   outflow
 FROM {{ ref("irr_total_privatepension") }}
+UNION ALL
+SELECT
+  entity_name,
+  --'companies' as ledger_name,
+  first_day_of_month,
+  value,
+  inflow,
+  outflow
+FROM {{ ref("irr_per_company") }}
+UNION ALL
+SELECT
+  entity_name,
+  --'underlying_products' as ledger_name,
+  first_day_of_month,
+  value,
+  inflow,
+  outflow
+FROM {{ ref("irr_per_underlying") }}
